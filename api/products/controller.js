62667d0b42ab6e157742ada4aa5196d872a90713
muhsinSutanto <sutantomuhsin@gmail.com>
2018-11-12 18:43:58 +0700

    CRUDS API

--- api/products/controller.js
+++ api/products/controller.js
@@ -12,3 +12,42 @@ module.exports.post = (req, res) => {
         .then(product => res.send(product))
         .catch(err => console.log(err))
 }
+
+module.exports.deleteOne = (req, res) => {
+    model.products.findOne({where: {id: req.params.id}})
+    .then(product => product.destroy()
+        .then(result => res.send(result))
+        .catch(err => console.log(err)))
+    .catch(err => console.log(err))
+}
+
+module.exports.deleteAll =  (req, res) => {
+    model.products.destroy({
+        where : {},
+        truncate: true
+    }).then(result => {
+        console.log(result)
+        if(result === 1){
+            res.send('success')}
+        else if (result === 0) {
+            res.send('failed')
+        }
+    }).catch( err => console.log('error')
+    )
+}
+
+module.exports.search = (req, res) => {
+    model.products.findAll({where: req.query})
+        .then(products => res.send(products))
+        .catch(err => console.log(err))
+}
+
+module.exports.update= (req, res) => {
+    model.products.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(result =>  res.send(result) )
+    .catch(err => console.log(err))
+}
+
