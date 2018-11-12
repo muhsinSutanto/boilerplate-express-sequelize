0c85b34729458207a2ff405dd95d352ee3705b24
muhsinSutanto <sutantomuhsin@gmail.com>
2018-11-12 16:12:20 +0700

    create

--- api/products/controller.js
+++ api/products/controller.js
@@ -2,11 +2,13 @@ const model = require("../models")
 
 module.exports.getAll = (req, res) => {
         model.products.findAll()
-            .then(products => console.log(products))
+            .then(products => res.send(products))
             .catch(err => console.log(err))
-        res.send("list of products")
+
 }
 
 module.exports.post = (req, res) => {
-        res.send("add new products")
+        model.products.create(req.body)
+        .then(product => res.send(product))
+        .catch(err => console.log(err))
 }
--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const express = require("express")
 
 const app = express()
 const products = require("./api/products")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+// add body parser
 
 app.use("/products", products)
 
